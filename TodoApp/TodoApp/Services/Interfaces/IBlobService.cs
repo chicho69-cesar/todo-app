13985981c1d5671ad4e7df847c6c1eda5caaa849
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-07-05 14:56:52 -0500

    Servicio de Imagenes agregado

---
+++ TodoApp/TodoApp/Services/Interfaces/IBlobService.cs
@@ -0,0 +1,8 @@
+﻿namespace TodoApp.Services.Interfaces {
+    public interface IBlobService {
+        Task<Guid> UploadBlobAsync(IFormFile file, string containerName);
+        Task<Guid> UploadBlobAsync(byte[] file, string containerName);
+        Task<Guid> UploadBlobAsync(string image, string containerName);
+        Task DeleteBlobAsync(Guid id, string containerName);
+    }
+}
