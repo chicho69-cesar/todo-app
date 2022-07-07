b7a74d5a5fc35b3289ed381357ce77a1218c8a58
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-07-07 14:35:10 -0500

    Dark theme agregado

---
+++ TodoApp/TodoApp/wwwroot/js/theme.js
@@ -0,0 +1,27 @@
+﻿const btnSwitch = document.querySelector('.switch-theme');
+
+setTheme = () => {
+    if (localStorage.getItem('theme') === null) {
+        document.body.classList.add('light');
+    } else {
+        let isDarkTheme = localStorage.getItem('theme') === 'dark';
+        document.body.classList.toggle('dark', isDarkTheme);
+        document.body.classList.toggle('light', !isDarkTheme);
+
+        btnSwitch.innerHTML = (localStorage.getItem('theme') === 'dark') 
+            ? '<i class="bi bi-sun-fill"></i>'
+            : '<i class="bi bi-moon-stars"></i>';
+    }
+}
+
+btnSwitch.addEventListener('click', () => {
+    if (document.body.classList.contains('dark')) {
+        localStorage.setItem('theme', 'light');
+    } else {
+        localStorage.setItem('theme', 'dark');
+    }
+
+    setTheme();
+});
+
+setTheme();
