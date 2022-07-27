e30dc37feb646e143e0b0dd455bad774c47124c0
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-07-26 22:05:40 -0500

    Inicio vistas de tareas en grupos

--- TodoApp/TodoApp/Controllers/TaskWorksController.cs
+++ TodoApp/TodoApp/Controllers/TaskWorksController.cs
@@ -1,7 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TodoApp.Services.Interfaces;
 
 namespace TodoApp.Controllers {
     public class TaskWorksController : Controller {
+        private readonly ITasksRepository _tasksRepository;
+
+        public TaskWorksController(
+            ITasksRepository tasksRepository
+        ) {
+            _tasksRepository = tasksRepository;
+        }
+
+        [HttpGet]
         public IActionResult Index() {
             return View();
         }
